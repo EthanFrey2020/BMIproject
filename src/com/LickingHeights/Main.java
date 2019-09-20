9a1b20bc8933128473e4502dcd8df5636b2011bf
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-09-20 09:59:53 -0400

    INITIALIZED THE VARIABLES WIT USER INPUT

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,18 +6,54 @@ public class Main {
 
     public static void main(String[] args) {
         // ToDo Declare Variables
-            double feet, inches, meters, kilograms, bmi;
+            double feet, inches, meters, kilograms, bmi, weight;
+
             double IN_TO_METERS = 0.0254;
+            double FT_TO_INCHES = 12;
+
             Scanner keyboard;
+
+
         //ToDo INITIALIZE THE VARIABLES WITH USER INPUT
+        keyboard = new Scanner(System.in);
+
+        System.out.println("What is your height in feet? (ex: x'6)");
+        feet = keyboard.nextDouble();
+
+        System.out.println("What is your height in inches? (ex: 5'x) ");
+        inches = keyboard.nextDouble();
+
+        System.out.println("What is your weight in pounds?");
+        weight = keyboard.nextDouble();
 
         //TODO CONVERT VARIABLES TO THE EQUATION
+            inches = inches  +(feet * 12);
             meters = inches * IN_TO_METERS;
+
+
+
         //TODO PERFORM BMI CALCULATION
+
 
         //TODO PRINT RESULTS
 
-        
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Your height in meters is...");
+
+        System.out.println("");
+
+
+
 
 
 
